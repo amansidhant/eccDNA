de5d36b24925057e59574b88ca33cc8d6641345d
amansidhant <31943078+amansidhant@users.noreply.github.com>
2018-12-01 20:11:50 -0800

    eccDNA files

---
+++ add.py
@@ -0,0 +1,9 @@
+#def accumu(lis):
+    #   total = 0
+    #for x in lis:
+    #   total += x
+#   yield total
+nums = [43270923, 35937250, 36413819, 35502694, 29958434, 31248787, 29697621, 28443022, 23012720, 23207287, 29021106]
+#print(list(accumu(nums)))
+b = sum(nums)
+print(b)
